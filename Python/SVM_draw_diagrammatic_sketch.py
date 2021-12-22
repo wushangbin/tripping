69b9c29255e711c42294d80e6178a67060daa38a
WuShangbin <595606350@qq.com>
2021-12-22 15:50:26 +0800

    完善了绘制SVM示意图的代码，增加了分割超平面绘制

--- Python/SVM_draw_diagrammatic_sketch.py
+++ Python/SVM_draw_diagrammatic_sketch.py
@@ -22,13 +22,21 @@ svc = LinearSVC(C=1.0)
 model = svc.fit(data_x, data_y)
 # --
 w = svc.coef_[0]
-k = - w[0]/w[1]
-xx = np.linspace(-2, 2)
-yy = k * xx - (svc.intercept_[0])/w[1]
+def draw_line(w0, w1, intercept_, **args):
+    """
+    根据权重绘制散点图"""
+    k = -w[0] / w[1]
+    xx = np.linspace(-2, 2)
+    yy = k*xx - (intercept_) / w[1]
+    plt.plot(xx, yy, **args)
+svm_w0, svm_w1, svm_intercept = svc.coef_[0][0], svc.coef_[0][1], svc.intercept_[0]
+draw_line(svm_w0, svm_w1, svm_intercept, linestyle = '-', color = 'blue', label="Separating Hyperplane with max margin")
+draw_line(svm_w0, svm_w1, svm_intercept-0.5, linestyle = '--', color = 'blue',)
+draw_line(svm_w0, svm_w1, svm_intercept+0.5, linestyle = '--', color = 'blue', label="Separating Hyperplane")
 # 绘制散点图
-plt.plot(xx, yy)
-plt.plot(gaussian1[:,0],gaussian1[:,1],'+', color='r')
-plt.plot(gaussian2[:,0],gaussian2[:,1],'o', color='g')
+plt.plot(gaussian1[:,0],gaussian1[:,1],'+', color='r', )
+plt.plot(gaussian2[:,0],gaussian2[:,1],'o', color='g', )
+plt.legend()
 plt.show()
 
 
